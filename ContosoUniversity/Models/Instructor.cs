64a15d18166a3042db492717946bae5673d60fbb
SiGua <1058435977@qq.com>
2019-05-29 15:13:35 +0800

    1. 创建 Instructor 实体, 在 Student 和 Instructor 实体中有几个属性是相同的。 本系列后面的实现继承教程将重构此代码以消除冗余。

---
+++ ContosoUniversity/Models/Instructor.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ContosoUniversity.Models
+{
+    public class Instructor
+    {
+        public int ID { get; set; }
+
+        [Required]
+        [Display(Name = "Last Name")]
+        [StringLength(50)]
+        public string LastName { get; set; }
+
+        [Required]
+        [Column("FirstName")]
+        [Display(Name = "First Name")]
+        [StringLength(50)]
+        public string FirstMidName { get; set; }
+
+        [Display(Name = "Full Name")]
+        public string FullName
+        {
+            get { return LastName + ", " + FirstMidName; }
+        }
+
+        public ICollection<CourseAssignment> CourseAssignments { get; set; }
+        public OfficeAssignment OfficeAssignment { get; set; }
+    }
+}
